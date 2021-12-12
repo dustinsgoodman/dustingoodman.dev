8b7a82f8f0fb52e3a99368bc3a45954bf6ab1abe
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-12-11 21:16:37 -0600

    add sun and moon icons

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -1,3 +1,5 @@
 export { default as DevToIcon } from './DevToIcon';
 export { default as GithubIcon } from './GithubIcon';
+export { default as MoonIcon } from './MoonIcon';
+export { default as SunIcon } from './SunIcon';
 export { default as TwitterIcon } from './TwitterIcon';
