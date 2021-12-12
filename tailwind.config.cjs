c325b69e2ef6442ee71b86435bfe8e68732827b6
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-12-11 20:59:21 -0600

    update tailwind config to use class instead of system preference

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {},
   plugins: [],
 };
