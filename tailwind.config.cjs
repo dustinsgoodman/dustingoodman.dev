c33aea6ce08b4e3d17e213adc88052246161c850
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-12-11 19:55:41 -0600

    remove color overrides and opt into defaults

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,20 +2,6 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      blue: colors.blue,
-      sky: colors.sky,
-      white: colors.white,
-      gray: colors.neutral,
-      indigo: colors.indigo,
-      red: colors.rose,
-      yellow: colors.amber,
-      purple: colors.violet,
-    },
-  },
+  theme: {},
   plugins: [],
 };
