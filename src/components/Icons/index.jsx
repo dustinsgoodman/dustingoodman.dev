a3f0e5280e60fc427fe18b854afb0eee9e1864e2
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-12-30 12:57:19 -0600

    add TwitchIcon

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -5,4 +5,5 @@ export { default as MicIcon } from './MicIcon';
 export { default as MoonIcon } from './MoonIcon';
 export { default as PencilIcon } from './PencilIcon';
 export { default as SunIcon } from './SunIcon';
+export { default as TwitchIcon } from './TwitchIcon';
 export { default as TwitterIcon } from './TwitterIcon';
