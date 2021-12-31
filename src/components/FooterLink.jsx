9869bbaa6cb0ee5ebd983a3616f1ecb2a2cc49cc
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-12-31 17:10:01 -0600

    migrate blue-500 to blue-600

--- src/components/FooterLink.jsx
+++ src/components/FooterLink.jsx
@@ -3,7 +3,7 @@ function FooterLink({ href, children, ...rest }) {
     <a
       {...rest}
       href={href}
-      className="block p-1 text-gray-500 dark:text-blue-100 hover:text-blue-500 focus:text-blue-500 dark:hover:text-sky-400 dark:focus:text-sky-400 text-base uppercase no-underline border-b border-transparent hover:border-blue-500 dark:hover:border-sky-400"
+      className="block p-1 text-gray-500 dark:text-blue-100 hover:text-blue-600 focus:text-blue-600 dark:hover:text-sky-400 dark:focus:text-sky-400 text-base uppercase no-underline border-b border-transparent hover:border-blue-600 dark:hover:border-sky-400"
     >
       {children}
     </a>
