4c544bab9f7b76fb1dc687fb44d14faad6881a0d
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-04-13 21:56:51 -0500

    apply 0.26 migrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,26 +11,14 @@ import { imagetools } from 'vite-imagetools';
 import preact from '@astrojs/preact';
 
 export default /** @type {import('astro').AstroUserConfig} */ ({
-  buildOptions: {
-    site: 'https://dustingoodman.dev/',
-  },
-  markdownOptions: {
-    render: [
-      '@astrojs/markdown-remark',
-      {
-        remarkPlugins: [
-          // Add a Remark plugin that you want to enable for your project.
-          // If you need to provide options for the plugin, you can use an array and put the options as the second item.
-          // ['remark-autolink-headings', { behavior: 'prepend'}],
-        ],
-        rehypePlugins: [
-          // Add a Rehype plugin that you want to enable for your project.
-          // If you need to provide options for the plugin, you can use an array and put the options as the second item.
-          // 'rehype-slug',
-          ['rehype-autolink-headings', { behavior: 'prepend' }],
-          ['rehype-external-links', { target: '_blank', rel: ['nofollow'] }],
-        ],
-      },
+  site: 'https://dustingoodman.dev/',
+  markdown: {
+    rehypePlugins: [
+      // Add a Rehype plugin that you want to enable for your project.
+      // If you need to provide options for the plugin, you can use an array and put the options as the second item.
+      // 'rehype-slug',
+      ['rehype-autolink-headings', { behavior: 'prepend' }],
+      ['rehype-external-links', { target: '_blank', rel: ['nofollow'] }],
     ],
   },
   integrations: [preact()],
