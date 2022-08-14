6aec0ce617169092420a77c3aad79feb11520ba6
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-08-14 10:30:35 -0500

    add learn page layout elements

---
+++ src/utils/unescapeHtml.ts
@@ -0,0 +1,8 @@
+export function unescapeHtml(escapedString: string) {
+  return escapedString
+    .replace(/&amp;/g, '&')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&quot;/g, '"')
+    .replace(/&#039;/g, "'");
+}
