091d931402531cd2840d30f98dfc90169a7a7290
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-08-15 19:37:50 -0500

    run formatter

--- src/components/Icons/CodeIcon.jsx
+++ src/components/Icons/CodeIcon.jsx
@@ -1,17 +1,17 @@
 function CodeIcon({ className = '' }) {
-  return (
-    <svg
-      className={className}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width="1em"
-      height="1em"
-      fill="currentColor"
-    >
-      <path fill="none" d="M0 0h24v24H0z" />
-      <path d="M23 12l-7.071 7.071-1.414-1.414L20.172 12l-5.657-5.657 1.414-1.414L23 12zM3.828 12l5.657 5.657-1.414 1.414L1 12l7.071-7.071 1.414 1.414L3.828 12z" />
-    </svg>
-  );
+	return (
+		<svg
+			className={className}
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			width="1em"
+			height="1em"
+			fill="currentColor"
+		>
+			<path fill="none" d="M0 0h24v24H0z" />
+			<path d="M23 12l-7.071 7.071-1.414-1.414L20.172 12l-5.657-5.657 1.414-1.414L23 12zM3.828 12l5.657 5.657-1.414 1.414L1 12l7.071-7.071 1.414 1.414L3.828 12z" />
+		</svg>
+	);
 }
 
 export default CodeIcon;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,13 @@
 export type BlogPost = {
-  title: string;
-  description: string;
-  publishDate: string;
-  heroImage: string;
-  alt: string;
-  layout: string;
-  originalArticle?: string;
-  originalSource?: 'Medium' | 'ThisDot';
-  tags?: string[];
-  redirectUrl?: string;
-  url?: string;
+	title: string;
+	description: string;
+	publishDate: string;
+	heroImage: string;
+	alt: string;
+	layout: string;
+	originalArticle?: string;
+	originalSource?: 'Medium' | 'ThisDot';
+	tags?: string[];
+	redirectUrl?: string;
+	url?: string;
 };
