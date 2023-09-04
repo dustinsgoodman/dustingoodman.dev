20cbf9e925fd8ed4edfcde8b9539d010d71909ae
Dustin Goodman <dustin@thisdot.co>
2023-09-04 17:21:47 -0500

    install fonts the astro-tailwind way

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	content: [
 		'./public/**/*.html',
 		'./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue,md,mdx}',
 	],
 	darkMode: 'class',
-	theme: {},
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+				mono: ['Azeret Mono', ...defaultTheme.fontFamily.mono],
+			},
+		},
+	},
 	plugins: [],
 };
