2fbc29fb067a9664fcabc750df37600a0bc67d8a
Dustin Goodman <dustin.s.goodman@gmail.com>
2024-12-07 00:08:21 -0500

    fix rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,26 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
 
 export async function GET(context) {
 	const blog = await getCollection('blog');
-	console.log(blog);
+
+	const items = blog.map((post) => {
+		const content = sanitizeHtml(post.body, {
+			allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+		});
+
+		return {
+			title: post.data.title,
+			pubDate: post.data.date,
+			description: post.data.description,
+			link: post.data.originalArticle
+				? post.data.originalArticle
+				: `/blog/${post.id}/`,
+			content,
+			customData: `<category>${post.data.tags.join(', ')}</category>`,
+		};
+	});
 
 	return rss({
 		title: "dustingoodman.dev - Dustin Goodman's Blog",
@@ -14,19 +28,6 @@ export async function GET(context) {
 			'A blog about Engineering Leadership, Software Development, and Personal Growth',
 		site: context.site,
 		customData: `<language>en-us</language>`,
-		items: blog.map((post) => ({
-			title: post.data.title,
-			pubDate: post.data.date,
-			description: post.data.description,
-			content: sanitizeHtml(parser.render(post.body), {
-				allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
-			}),
-			customData: `
-        <category>${post.data.tags.join(', ')}</category>
-      `,
-			link: post.data.originalArticle
-				? post.data.originalArticle
-				: `/blog/${post.id}/`,
-		})),
+		items,
 	});
 }
